ae49336cc6204ce75b66232ec97eb2104190e778
Keren-Happuc Fleurimond <keren@noukod.com>
2019-03-22 15:13:30 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+
+  get '/auth', to: 'sessions#create'
+  get '/friends', to: 'searches#friends'
+
   root 'searches#search'
   get '/search', to: 'searches#search'
   post '/search', to: 'searches#foursquare'
-
 end
